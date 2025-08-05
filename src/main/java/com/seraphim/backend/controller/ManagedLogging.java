fc6f7581445f79f70620a2ede5bd2e2e6c4bd566
04megumi <wzy12241129@qq.com>
2025-08-05 10:03:06 +0800

    feat: 管理日志

---
+++ src/main/java/com/seraphim/backend/controller/ManagedLogging.java
@@ -0,0 +1,9 @@
+package com.seraphim.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("managed-logging")
+public class ManagedLogging {
+}
